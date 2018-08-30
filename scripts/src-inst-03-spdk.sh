ba5a5c4ffc02a5b3035d4884447ea08bdb657366
Simon A. F. Lund <safl@safl.dk>
2018-08-30 10:45:42 +0200

    scripts: fixed install of SPDK/DPDK

--- scripts/src-inst-03-spdk.sh
+++ scripts/src-inst-03-spdk.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 #
-# Installs SPDK tagged v18.04 from source into /opt/spdk
+# Installs SPDK tagged v18.07 from source into /opt/spdk
 #
 set -e
 
 NAME=spdk
 REPOS_URL=https://github.com/spdk/spdk.git
-REPOS_PATH=$(mktemp -d -u --suffix=-$NAME -t repos-XXXXXX)
+OPT_PATH=/opt/$NAME
+REPOS_PATH=$OPT_PATH
 REPOS_TAG="v18.07"
 
+echo "# NAME: '$NAME' REPOS_TAG: '$REPOS_TAG'"
 echo "# install '$NAME' from '$REPOS_URL' via '$REPOS_PATH'"
 
 git clone $REPOS_URL $REPOS_PATH
 cd $REPOS_PATH
 
-echo "# NAME: '$NAME' REPOS_TAG: '$REPOS_TAG'"
-OPT_PATH=/opt/$NAME
-
 git checkout .
 git checkout $REPOS_TAG
 
-make clean
 git submodule update --init
 ./scripts/pkgdep.sh
-make clean
-./configure --prefix=$OPT_PATH
-make
 
-cp -r $REPOS_PATH/. $OPT_PATH/
+./configure
+make -j $(nproc)
+make install
+
+cd dpdk
+make install
 
 echo "- {'name': '$NAME', 'tag': '$REPOS_TAG'}" >> /opt/ver.refenv.yml
 echo "# DONE: '$NAME'"
